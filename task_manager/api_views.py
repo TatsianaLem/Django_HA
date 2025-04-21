466264b67925f0885a479affc525e498f816f0f6
Tatsianal <tatsianalepiashko@gmail.com>
2025-04-21 12:37:27 +0200

    add api_views.py, urls.py/ task_manager

--- task_manager/api_views.py
+++ task_manager/api_views.py
@@ -1,23 +1,31 @@
-from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from task_manager.models import Task
 from django.db.models import Count
 from task_manager.serializers import TaskSerializer
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
+class TaskCreateAPIView(APIView):
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TaskCreateAPIView(generics.CreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+class TaskListAPIView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
-class TaskListAPIView(generics.ListAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-class TaskDetailAPIView(generics.RetrieveAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+class TaskDetailAPIView(APIView):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
 
 class TaskStatsView(APIView):
     def get(self, request):
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from task_manager.api_views import TaskCreateAPIView, TaskListAPIView, TaskDetailAPIView, TaskStatsView
+from task_manager.api_views import (
+    TaskCreateAPIView,
+    TaskListAPIView,
+    TaskDetailAPIView,
+    TaskStatsView,
+)
 
 urlpatterns = [
     path('tasks/create/', TaskCreateAPIView.as_view(), name='task-create'),
     path('tasks/', TaskListAPIView.as_view(), name='task-list'),
     path('tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='task-detail'),
-    path('tasks/status/', TaskStatsView.as_view(), name='task-stats'),
+    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
 ]
